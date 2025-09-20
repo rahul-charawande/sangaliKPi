a7cfb79d7b0fd2faee23b2d6e893d1e1f87cf823
rahul-charawande <rahul.charawande00786@gmail.com>
2025-09-20 20:05:57 +0530

    render deploy

--- server.js
+++ server.js
@@ -102,7 +102,6 @@ app.post("/api/fetch-excel", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
