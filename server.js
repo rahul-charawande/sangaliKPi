458556669d8a879eabd40491f30686928f17d171
rahul-charawande <rahul.charawande00786@gmail.com>
2025-09-20 18:08:13 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/api/fetch-excel", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
